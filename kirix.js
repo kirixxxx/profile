8977cea1ac5a934bc3ab10ab840e4d3de1dfd83d
kirixxxx <119788742+kirixxxx@users.noreply.github.com>
2022-12-05 10:48:16 +0600

    Add files via upload

---
+++ kirix.js
@@ -0,0 +1,66 @@
+﻿
+ var person = null;
+ if(confirm("Процесті жалғастырасыз ба?")){
+	 person = prompt("Есіміңізді жазыңыз");
+	 alert("Cәлем," + person);
+	 
+ } else{
+	 alert("Сіз процесті тоқтаттыңыз!");
+ }
+ 
+
+ 
+ (function (global) {
+    
+    function Clock(el) {
+        var document = global.document;
+        this.el = document.getElementById(el);
+        this.months = ['Қантар', 'Ақпан', 'Наурыз', 'Сәуір', 'Мамыр', 'Маусым', 'Шілде', 'Тамыз', 'Қыркүйек', 'Қазан', 'Қараша', 'Желтоқсан'];
+        this.days = ['Жексенбі', 'Дүйсенбі', 'Сейсенбі', 'Сәрсенбі', 'Бейсенбі', 'Жұма', 'Сенбі'];
+    }
+    Clock.prototype.addZero = function (i) {
+        if (i < 10) {
+            i = "0" + i;
+            return i;
+        }
+        return i;
+    };
+    Clock.prototype.updateClock = function () {
+        var now, year, month, dayNo, day, hour, minute, second, result, self;
+        now = new global.Date();
+        year = now.getFullYear();
+        month = now.getMonth();
+        dayNo = now.getDay();
+        day = now.getDate();
+        hour = this.addZero(now.getHours());
+        minute = this.addZero(now.getMinutes());
+        second = this.addZero(now.getSeconds());
+        result = this.days[dayNo] + ", " + day + " " + this.months[month] + " " + year + " " + hour + ":" + minute + ":" + second;
+        self = this;
+        self.el.innerHTML = result;
+        global.setTimeout(function () {
+            self.updateClock();
+        }, 1000);
+    };
+    global.Clock = Clock;
+}(window));
+
+function addEvent(elm, evType, fn, useCapture) {
+
+    if (elm.addEventListener) {
+        elm.addEventListener(evType, fn, useCapture);
+    } else if (elm.attachEvent) {
+        elm.attachEvent('on' + evType, fn);
+    } else {
+        elm['on' + evType] = fn;
+    }
+}
+
+addEvent(window, "load", function () {
+    if (document.getElementById("clock")) {
+        var clock = new Clock("clock");
+        clock.updateClock();
+    }
+});
+ 
+
